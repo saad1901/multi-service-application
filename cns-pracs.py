45bd0bfbbb9251b75c889f41e346947913483061
saadWindows <saadiqbal1921@gmail.com>
2024-03-20 23:23:59 +0530

    Auto Committed

--- cns-pracs.py
+++ cns-pracs.py
@@ -213,7 +213,7 @@ elif selection == 'Gallery':
         files = os.listdir(current_directory)
         image_files = [file for file in files if file.endswith(('png', 'jpg', 'jpeg', 'gif'))]
         return image_files
-    st.title("Local Image Viewer")
+    st.title("Image Gallery")
     image_files = list_images()
     if not image_files:
         st.write("No images found in the current directory.")
