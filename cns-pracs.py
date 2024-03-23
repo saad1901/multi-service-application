ba87bc3e251c6720cfe4a7b7f0a22e300ec8cbb0
saadWindows <saadiqbal1921@gmail.com>
2024-03-23 22:48:16 +0530

    Auto Committed

--- cns-pracs.py
+++ cns-pracs.py
@@ -247,7 +247,7 @@ elif selection == 'Cloud Storage':
         # st.info("No files uploaded yet!")
         pass
 
-elif selection == 'add images':
+elif selection == 'add Images':
     def save_captured_photo(uploaded_file):
         filename = f"FromWeb_{int(time.time())}.jpg"
         filepath = os.path.join(os.getcwd(), filename)
